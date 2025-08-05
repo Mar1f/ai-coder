4640e72576f24f492788d76d57936c6b76c88d06
Mar1f <938224402@qq.com>
2025-08-05 13:25:12 +0800

    Add HealthController, update config to YAML, add knife4j,openapi,hutool,lombok...dependencies

---
+++ src/main/java/com/mar/aicoder/controller/HealthController.java
@@ -0,0 +1,20 @@
+package com.mar.aicoder.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description；
+ * @author:mar1
+ * @data:2025/08/05
+ **/
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "ok";
+    }
+}
